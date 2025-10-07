07ae83d041d1805599802c22deebcf23dbf1936c
luizfelipe798 <luizvignoto@gmail.com>
2025-10-07 00:07:40 -0300

    Programação de novos arquivos e de novas linhas de codificação no arquivo "index.html"

--- index.php
+++ index.php
@@ -8,6 +8,38 @@
     <title>Lista de usuários</title>
 </head>
 <body>
-    <?php include 'usuarios.php'; ?>
+    <?php 
+        include 'menu.php'; 
+    ?>
+
+    <div class="conteudoprincipal-container">
+        <div class="titulo-container">
+            <h1>Lista de usuários</h1>
+        </div>
+        <div class="btn_adicionar-container">
+            <a href="">Adicionar</a>
+        </div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Data de nascimento</th>
+                    <th>Sexo</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Luiz</td>
+                    <td>luiz@gmail.com</td>
+                    <td>29/03/2008</td>
+                    <td>Masculino</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body> 
 </html> 
