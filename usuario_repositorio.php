b49c3c391609339683b67100de307c98c5ea8a2e
luizfelipe798 <luizvignoto@gmail.com>
2025-10-08 19:09:13 -0300

    Continuação da programação

--- usuario_repositorio.php
+++ usuario_repositorio.php
@@ -1,3 +1,18 @@
 <?php
+    $acao = $_POST['acao'];
 
+    switch($acao)
+    {
+        case "cadastro":
+
+        break;
+
+        case "edicao":
+
+        break;
+
+        case "delecao":
+
+        break;
+    }
 ?>
